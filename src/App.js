68a37e9711217546120de850a264ec0c836c260b
Kim <kim.nguyen4124@gmail.com>
2022-01-04 14:43:45 -0800

    added date components and cleaned

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Rent",
+      amount: 553,
+      date: new Date(2022, 0, 3),
+    },
+    { id: "e2", title: "Phone Plan", amount: 50, date: new Date(2022, 0, 3) },
+    {
+      id: "e3",
+      title: "Prescription",
+      amount: 30,
+      date: new Date(2022, 0, 4),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2022, 0, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
