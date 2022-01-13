81fd80573d2904200fd4646145c9be8629cf4e5c
kim <kim.nguyen4124@gmail.com>
2022-01-12 16:30:45 -0800

    updated n added components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -17,35 +19,20 @@ function App() {
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2022, 0, 420),
+      title: "Electricity",
+      amount: 63.13,
+      date: new Date(2022, 0, 11),
     },
   ];
 
   return (
+  
+
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <NewExpense/>
+
+      <Expenses items = {expenses}/>
+
     </div>
   );
 }
